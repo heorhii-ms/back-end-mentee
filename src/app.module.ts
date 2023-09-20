a8bf489f33d1db10db218370d807cb252c0c14f1
heorhii.ms <heorhii.ms@darly.solutions>
2023-09-20 19:15:00 +0300

    feat: add ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 
 import {UserModule} from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
+import {Logger} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
 import {NestFactory} from '@nestjs/core';
 
 import {AppModule} from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const serverHost = config.get<string>('API_HOST') || 'http://localhost';
+  const serverPort = config.get<number>('API_PORT') || 3000;
+  const rootServerPath = `${serverHost}:${serverPort}`;
+
+  await app.listen(serverPort, () => {
+    logger.log(`
+    Server:
+    The server runs on: ${rootServerPath}
+    `);
+  });
 }
 bootstrap();
