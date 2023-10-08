af4b83b8211e1b8dbb2dddefc1027c88f8e522c0
heorhii.ms <heorhii.ms@darly.solutions>
2023-10-08 22:28:25 +0300

    fix: add config env file settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import {UserModule} from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: [process.env.STAGE ? `.env.${process.env.STAGE}` : '.env'],
       isGlobal: true,
     }),
     UserModule,
